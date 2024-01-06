458c7cf4ecdbc4add828195b541c41f3c8f534bb
d3drock <61801183+d3drock@users.noreply.github.com>
2024-01-06 17:06:25 +0530

    feat: implement auth ui

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -1,21 +1,23 @@
 import Link from "next/link";
 import MaxWidthWrapper from "../MaxWidthWrapper";
 import Image from "next/image";
-import logo from "../../../public/logo.PNG";
+import { buttonVariants } from "../ui/button";
 const Navbar = () => {
   return (
     <nav className='sticky h-14 inset-x-0 top-0 border-b border-gray-200 bg-white/50 backdrop-blur-lg transition-all  '>
       <MaxWidthWrapper>
-        <div className='flex h-14 items-center justify-center border-green-[#2f433a]'>
-          <Link href={"/"}>
-            <Image src={logo} alt='logo' width={"64"} height={"64"} />
+        <div className='flex h-14 items-center justify-between border-green-[#2f433a]'>
+          <Link href={"/"} className='flex z-40 '>
+            <Image
+              src={"/logo.PNG"}
+              alt='logo'
+              className='p-2'
+              width={"100"}
+              height={"100"}
+            />
           </Link>
           <div className='hidden items-center space-x-4 sm:flex '>
-            <>
-              <Link href={"/"} className={""}>
-                there
-              </Link>
-            </>
+            <></>
           </div>
         </div>
       </MaxWidthWrapper>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
-import { height } from "@mui/system";
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -15,11 +17,7 @@ const config: Config = {
         "2xl": "1400px",
       },
     },
-
     extend: {
-      maxWidth: {
-        "8xl": "1408px",
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -57,17 +55,26 @@ const config: Config = {
       },
       borderRadius: {
         lg: "var(--radius)",
-        md: "calc(var(--radius)- 2px)",
-        sm: "calc(var(--radius) -4px)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+} satisfies Config;
+
 export default config;
