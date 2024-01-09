269d934c834a1fbc778b62a48e7382f2c5e6316e
souvikdeb2612 <souvikdeb26@gmail.com>
2024-01-09 21:43:22 +0530

    refactor: update the folder structure

---
+++ src/app/_components/mobile-student-navbar 2.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Image from "next/image";
+import { SidebarRoutes } from "./sidebar-routes";
+import { StudentSidebarRoutes } from "./student-navbar-routes";
+
+function MobileStudentSidebar() {
+  return (
+    <div className="h-full flex align-center flex-col overflow-y-auto bg-white">
+      {/* <div className="p-6"> */}
+      <Image
+        src={"/logo.PNG"}
+        alt="logo"
+        // className="p-2"
+        width={"60"}
+        height={"60"}
+      />
+      {/* </div> */}
+      <div className="flex h-full flex-col w-full mt-3">
+        <StudentSidebarRoutes />
+      </div>
+    </div>
+  );
+}
+
+export default MobileStudentSidebar;
