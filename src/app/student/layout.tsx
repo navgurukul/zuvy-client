ff015761ed4344f0493a520f750934f453435e3d
souvikdeb2612 <souvikdeb26@gmail.com>
2024-01-14 13:04:53 +0530

    refactor: change app directory to page

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -1,9 +1,15 @@
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
+<<<<<<<< HEAD:src/pages/student/layout.tsx
 import "../globals.css";
 import StudentSidebar from "@/app/_components/student-sidebar";
 import StudentNavbar from "@/app/_components/student-navbar";
+========
+import "../../globals.css";
+import StudentSidebar from "@/pages/_components/student-sidebar";
+import StudentNavbar from "@/pages/_components/student-navbar";
+>>>>>>>> 5de397e (refactor: change app directory to page):src/pages/(dashboard)/student-dashboard/layout.tsx
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
