e484a4b8eb166f6c0ddbc7ca589f4d1797c1c959
Nilatpal <nilatpaldas60@gmail.com>
2024-01-17 12:25:22 +0530

    initial commit

--- src/app/(dashboard)/admin/_components/main/courses/courses.tsx
+++ src/app/(dashboard)/admin/_components/main/courses/courses.tsx
@@ -19,17 +19,17 @@ interface Course {
   learnersCount: number;
   date: string;
   image: string; // URL for the course image
-  id: string; 
+  id: string;
 }
 
-
-
 const Courses: React.FC = () => {
-  const [activeFilter, setActiveFilter] = useState<"all" | "active" | "completed">("all");
+  const [activeFilter, setActiveFilter] = useState<
+    "all" | "active" | "completed"
+  >("all");
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [courses, setCourses] = useState<Course[]>([]);
   const [newCourseName, setNewCourseName] = useState<string>("");
-  const MAIN_URL = process.env.MAIN_URL
+  const MAIN_URL = process.env.MAIN_URL;
   const handleFilterClick = (filter: "all" | "active" | "completed") => {
     setActiveFilter(filter);
   };
@@ -38,46 +38,49 @@ const Courses: React.FC = () => {
     setSearchQuery(event.target.value);
   };
 
-  const handleNewCourseNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleNewCourseNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setNewCourseName(event.target.value);
   };
 
   const handleCreateCourse = () => {
     fetch(`${MAIN_URL}/bootcamp`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: newCourseName,
       }),
     })
-      .then(response => response.json())
-      .then(data => {
-        setCourses(prevCourses => [...prevCourses, data]);
+      .then((response) => response.json())
+      .then((data) => {
+        setCourses((prevCourses) => [...prevCourses, data]);
       })
-      .catch(error => console.error('Error creating course:', error));
+      .catch((error) => console.error("Error creating course:", error));
   };
 
   useEffect(() => {
     fetch(`${MAIN_URL}/bootcamp`)
-      .then(response => response.json())
-      .then(data => setCourses(data))
-      .catch(error => console.error('Error fetching courses:', error));
+      .then((response) => response.json())
+      .then((data) => setCourses(data))
+      .catch((error) => console.error("Error fetching courses:", error));
   }, []);
 
   // const handleCourseClick = (courseId: string) => {
   //   window.location.href = `course/${courseId}`;
   // };
 
-  const filteredCourses = courses.filter((course) =>
-    course.name && course.name.toLowerCase().includes(searchQuery.toLowerCase())
+  const filteredCourses = courses.filter(
+    (course) =>
+      course.name &&
+      course.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
   // const handleCardClick = (name: string, id: string) => {
   //   onMenuItemClick(name);
   //   courseId(id);
   // };
-  
 
   return (
     <div>
@@ -93,21 +96,32 @@ const Courses: React.FC = () => {
           />
           <Dialog>
             <DialogTrigger>
-              <Button className={styles.newCourseBtn}>
-                {" "}
-                + New Course
-              </Button>
+              <Button className={styles.newCourseBtn}> + New Course</Button>
             </DialogTrigger>
             <DialogOverlay />
             <DialogContent>
               <DialogHeader>
-                <DialogTitle className={styles.newCourse}>New Course</DialogTitle>
+                <DialogTitle className={styles.newCourse}>
+                  New Course
+                </DialogTitle>
                 <DialogDescription>
-                  <div style={{ marginBottom: '10px' }}>
-                    <label htmlFor="name" className={styles.dialogname}>Course Name:</label>
-                    <input type="text" id="name" placeholder="Enter course name" className={styles.inputBox} value={newCourseName} onChange={handleNewCourseNameChange}/>
+                  <div style={{ marginBottom: "10px" }}>
+                    <label htmlFor="name" className={styles.dialogname}>
+                      Course Name:
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      placeholder="Enter course name"
+                      className={styles.inputBox}
+                      value={newCourseName}
+                      onChange={handleNewCourseNameChange}
+                    />
                   </div>
-                  <Button onClick={() => handleCreateCourse()} className={styles.createCourseBtnDialog}>
+                  <Button
+                    onClick={() => handleCreateCourse()}
+                    className={styles.createCourseBtnDialog}
+                  >
                     Create Course
                   </Button>
                 </DialogDescription>
@@ -146,43 +160,58 @@ const Courses: React.FC = () => {
         <div className={styles.courceContainer}>
           {filteredCourses.length === 0 ? (
             <div className={styles.centeredContainer}>
-              <h4 className={styles.firstCourseText}>Create your first course and share with students</h4>
+              <h4 className={styles.firstCourseText}>
+                Create your first course and share with students
+              </h4>
               <Dialog>
-            <DialogTrigger>
-              <Button className={styles.newCourseBtn}>
-                {" "}
-                + New Course
-              </Button>
-            </DialogTrigger>
-            <DialogOverlay />
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle className={styles.newCourse}>New Course</DialogTitle>
-                <DialogDescription>
-                  <div style={{ marginBottom: '10px' }}>
-                    <label htmlFor="name" className={styles.dialogname}>Course Name:</label>
-                    <input type="text" id="name" placeholder="Enter course name" className={styles.inputBox} value={newCourseName} onChange={handleNewCourseNameChange}/>
-                  </div>
-                  <Button onClick={() => handleCreateCourse()} className={styles.createCourseBtnDialog}>
-                    Create Course
-                  </Button>
-                </DialogDescription>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
+                <DialogTrigger>
+                  <Button className={styles.newCourseBtn}> + New Course</Button>
+                </DialogTrigger>
+                <DialogOverlay />
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className={styles.newCourse}>
+                      New Course
+                    </DialogTitle>
+                    <DialogDescription>
+                      <div style={{ marginBottom: "10px" }}>
+                        <label htmlFor="name" className={styles.dialogname}>
+                          Course Name:
+                        </label>
+                        <input
+                          type="text"
+                          id="name"
+                          placeholder="Enter course name"
+                          className={styles.inputBox}
+                          value={newCourseName}
+                          onChange={handleNewCourseNameChange}
+                        />
+                      </div>
+                      <Button
+                        onClick={() => handleCreateCourse()}
+                        className={styles.createCourseBtnDialog}
+                      >
+                        Create Course
+                      </Button>
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
               <hr className={styles.hrLine} />
-              <p className={styles.needHelpText}>Need help getting started? Checkout the tutorials below</p>
+              <p className={styles.needHelpText}>
+                Need help getting started? Checkout the tutorials below
+              </p>
             </div>
           ) : (
             filteredCourses.map((course, index) => (
               <Card
                 key={index}
                 className={styles.cardContainer}
-                onClick={() => handleCardClick(course.name, course.id)}
+                // onClick={() => handleCardClick(course.name, course.id)}
               >
                 <div className={styles.courseImageContainer}>
                   <img
-                    src='https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg'
+                    src="https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg"
                     alt={`Course: ${course.name}`}
                     className={styles.courseImage}
                   />
@@ -191,7 +220,9 @@ const Courses: React.FC = () => {
                   <span className={styles.nameText}>{course.name}</span>
                 </div>
                 <div className={styles.courseDetails}>
-                  <span className={styles.learnersCount}>{course.learnersCount} Learners</span>
+                  <span className={styles.learnersCount}>
+                    {course.learnersCount} Learners
+                  </span>
                   <span>{course.date}</span>
                 </div>
               </Card>
