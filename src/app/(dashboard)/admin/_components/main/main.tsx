9afc740a5cd33500177d0d96170dc89246b701fe
Nilatpal <nilatpaldas60@gmail.com>
2024-01-17 12:25:22 +0530

    initial commit

--- src/app/(dashboard)/admin/_components/main/main.tsx
+++ src/app/(dashboard)/admin/_components/main/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import HomeContent from './homeContent/homeContent';
-import Notifications from './notifications/notifications';
-import Courses from './courses/courses';
-import CourseDetails from './courses/courseDetails';
-import RecourceBank from './recourceBank/recourceBank';
-import Instructors from './instructors/instructors';
-import Reports from './reports/reports';
-import Settings from './settings/settings';
-import Help from './help/help';
+import React from "react";
+import HomeContent from "./homeContent/homeContent";
+import Notifications from "./notifications/notifications";
+import Courses from "./courses/courses";
+import CourseDetails from "./courses/courseDetails";
+import RecourceBank from "./recourceBank/recourceBank";
+import Instructors from "./instructors/instructors";
+import Reports from "./reports/reports";
+import Settings from "./settings/settings";
+import Help from "./help/help";
 
 interface MainContentProps {
   selectedMenuItem: string;
@@ -17,9 +17,9 @@ const MainContent: React.FC<MainContentProps> = ({ selectedMenuItem }) => {
   // Define the mapping between menu items and their corresponding content components
   const contentMap: { [key: string]: React.ReactNode } = {
     Home: <HomeContent />,
-    Notifications: <Notifications/>,
+    Notifications: <Notifications />,
     Courses: <Courses />,
-    'Resource Bank': <RecourceBank />,
+    "Resource Bank": <RecourceBank />,
     Instructors: <Instructors />,
     Reports: <Reports />,
     Settings: <Settings />,
@@ -27,11 +27,7 @@ const MainContent: React.FC<MainContentProps> = ({ selectedMenuItem }) => {
   };
 
   // Render the selected content based on the menu item
-  return (
-    <div className="main-content">
-      {contentMap[selectedMenuItem]}
-    </div>
-  );
+  return <div className="main-content">{contentMap[selectedMenuItem]}</div>;
 };
 
 export default MainContent;
--- src/app/(dashboard)/admin/_components/sidebar/sidebar.tsx
+++ src/app/(dashboard)/admin/_components/sidebar/sidebar.tsx
@@ -1,53 +1,64 @@
 import React from "react";
-import { Home, Bell, Book, Database, User, BarChart, Cog, HelpCircle } from 'lucide-react';
+import {
+  Home,
+  Bell,
+  Book,
+  Database,
+  User,
+  BarChart,
+  Cog,
+  HelpCircle,
+} from "lucide-react";
 import styles from "./sidebar.module.css";
 import Image from "next/image";
 
 interface SidebarProps {
-    onMenuItemClick: (menuItem: string) => void;
-    selectedMenuItem: string;
+  onMenuItemClick: (menuItem: string) => void;
+  selectedMenuItem: string;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ onMenuItemClick, selectedMenuItem }) => {
-    const menuItems = [
-        { name: 'Home', icon: <Home /> },
-        { name: 'Notifications', icon: <Bell /> },
-        { name: 'Courses', icon: <Book /> },
-        { name: 'Resource Bank', icon: <Database /> },
-        { name: 'Instructors', icon: <User /> },
-        { name: 'Reports', icon: <BarChart /> },
-        { name: 'Settings', icon: <Cog /> },
-        { name: 'Help', icon: <HelpCircle /> },
-    ];
+const Sidebar: React.FC<SidebarProps> = ({
+  onMenuItemClick,
+  selectedMenuItem,
+}) => {
+  const menuItems = [
+    { name: "Home", icon: <Home /> },
+    { name: "Notifications", icon: <Bell /> },
+    { name: "Courses", icon: <Book /> },
+    { name: "Resource Bank", icon: <Database /> },
+    { name: "Instructors", icon: <User /> },
+    { name: "Reports", icon: <BarChart /> },
+    { name: "Settings", icon: <Cog /> },
+    { name: "Help", icon: <HelpCircle /> },
+  ];
 
-    return (
-        <div className={styles.sidebar}>
-            <div className={styles.logoContainer}>
-              
-                <Image
-                    src={"/logo.PNG"}
-                    alt="logo"
-                    // className="p-2"
-                    width={"60"}
-                    height={"60"}
-                />
-                
-            </div>
-            <div>
-                {menuItems.map((item, index) => (
-                    <div
-                        key={index}
-                        className={`${styles.sidebarItem} ${item.name === selectedMenuItem ? styles.selectedItem : ""
-                            }`}
-                        onClick={() => onMenuItemClick(item.name)}
-                    >
-                        <span className={styles.sidebarIcon}>{item.icon}</span>
-                        <span className={styles.sidebarItemText}>{item.name}</span>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.logoContainer}>
+        <Image
+          src={"/logo.PNG"}
+          alt="logo"
+          // className="p-2"
+          width={"60"}
+          height={"60"}
+        />
+      </div>
+      <div>
+        {menuItems.map((item, index) => (
+          <div
+            key={index}
+            className={`${styles.sidebarItem} ${
+              item.name === selectedMenuItem ? styles.selectedItem : ""
+            }`}
+            onClick={() => onMenuItemClick(item.name)}
+          >
+            <span className={styles.sidebarIcon}>{item.icon}</span>
+            <span className={styles.sidebarItemText}>{item.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
