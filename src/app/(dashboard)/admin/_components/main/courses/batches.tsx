818baab0d9f95c304d48fc63189ece26322fbcbb
rohith420 <139437364+rohithsigma6@users.noreply.github.com>
2024-01-22 11:06:40 +0530

    Updated create branch dialog box

--- src/app/(dashboard)/admin/_components/main/courses/batches.tsx
+++ src/app/(dashboard)/admin/_components/main/courses/batches.tsx
@@ -1,6 +1,3 @@
-
-
-
 import React, { useState } from "react";
 import styles from "./cources.module.css";
 import { Input } from "@/components/ui/input";
@@ -24,60 +21,99 @@ interface Course {
   image: string; // URL for the course image
 }
 
-  const defaultCourses: Course[] = [
-    {
-      groupName: 'Group1',
-      learnersCount: 67,
-      date: '2022-01-12',
-      image: 'https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg',
-    },
-    {
-      groupName: 'Group2',
-      learnersCount: 67,
-      date: '2022-02-20',
-      image: 'https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg',
-    },
-    {
-      groupName: 'Group3',
-      learnersCount: 67,
-      date: '2022-01-12',
-      image: 'https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg',
-    },
-  
-  ]
+const defaultCourses: Course[] = [
+  {
+    groupName: 'Group1',
+    learnersCount: 67,
+    date: '2022-01-12',
+    image: 'https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg',
+  },
+  {
+    groupName: 'Group2',
+    learnersCount: 67,
+    date: '2022-02-20',
+    image: 'https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg',
+  },
+  {
+    groupName: 'Group3',
+    learnersCount: 67,
+    date: '2022-01-12',
+    image: 'https://t4.ftcdn.net/jpg/03/78/40/11/360_F_378401105_9LAka9cRxk5Ey2wwanxrLTFCN1U51DL0.jpg',
+  },
+
+]
 
 const Batches: React.FC = () => {
   const [activeFilter, setActiveFilter] = useState<"all" | "active" | "completed">("all");
-
+  const [cap, setCap] = useState<number | null>(null);
   const handleFilterClick = (filter: "all" | "active" | "completed") => {
     setActiveFilter(filter);
+  };
+  const handleCapChange = (capacity: number) => {
+    setCap(capacity);
   };
 
   return (
     <div>
-     
+
       <div>
         <div className={styles.searchContainer}>
           <Input type="text" placeholder="Search" className={styles.searchInput} />
 
           <Dialog>
             <DialogTrigger>
-            <Button variant="outline" className={styles.newCourseBtn}>
-            {" "}
-            + New Batch
-          </Button>
+              <Button className={styles.newCourseBtn}>
+                {" "}
+                + New Batch
+              </Button>
             </DialogTrigger>
-            <DialogOverlay/>
+            <DialogOverlay />
             <DialogContent>
               <DialogHeader>
-                <DialogTitle>New Batch</DialogTitle>
-                <DialogDescription>
-                  This action cannot be undone. This will permanently delete your account
-                  and remove your data from our servers.
+                <DialogTitle className={styles.newCourse}>New Batch</DialogTitle>
+                <DialogDescription >
+                  <div style={{ marginBottom: '10px' }}>
+                    <label htmlFor="batchName" className={styles.dialogCourseName}>Batch Name:</label>
+                    <input type="text" id="batchName" placeholder="Enter batch name" className={styles.inputBox} />
+                    <label htmlFor="instructor" className={styles.dialogCourseName}>Instructor:</label>
+                    <input type="text" id="instructor" placeholder="Enter Instructor name" className={styles.inputBox} />
+                    <label htmlFor="courseCommencement" className={styles.dialogCourseName}>Course Commencement:</label>
+                    <input type="date" id="courseCommencement" className={styles.inputBox} />
+                    <label htmlFor="capEnrollment" className={styles.label}>
+                      Cap Enrollment:
+                    </label>
+
+                    <div className={styles.durationButtons}>
+                      <button
+                        onClick={() => handleCapChange(50)}
+                        className={`${styles.durationButton} ${cap === 50 ? styles.active : ""
+                          }`}
+                      >
+                        50
+                      </button>
+                      <button
+                        onClick={() => handleCapChange(100)}
+                        className={`${styles.durationButton} ${cap === 100 ? styles.active : ""
+                          }`}
+                      >
+                        100
+                      </button>
+                      <button
+                        onClick={() => handleCapChange(150)}
+                        className={`${styles.durationButton} ${cap === 150 ? styles.active : ""
+                          }`}
+                      >
+                        150
+                      </button>
+                    </div>
+                  </div>
+                  <Button onClick={() => handleCreateBatch()} className={styles.createCourseBtnDialog}>
+                    Create Batch
+                  </Button>
                 </DialogDescription>
               </DialogHeader>
             </DialogContent>
-        </Dialog>
+          </Dialog>
         </div>
         <div className={styles.contentContainer}>
           <div className={styles.content}>
@@ -99,10 +135,10 @@ const Batches: React.FC = () => {
             >
               Completed
             </span>
-           
+
           </div>
           <div>
-        
+
           </div>
         </div>
         <div className={styles.courceContainer}>
