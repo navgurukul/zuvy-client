128058fbdbb6f702d753dc0854ea181f3a42720e
souvikdeb2612 <souvikdeb26@gmail.com>
2024-02-09 19:16:58 +0530

    feat: improve UX

--- src/app/_components/sidebar-item.tsx
+++ src/app/_components/sidebar-item.tsx
@@ -4,6 +4,7 @@ import { LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import { Logout } from "@/utils/logout";
 import { cn } from "@/lib/utils";
+import { SheetClose } from "@/components/ui/sheet";
 
 interface SidebarItemProps {
   icon: LucideIcon;
@@ -28,28 +29,30 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
   };
 
   return (
-    <button
-      onClick={onClick}
-      type="button"
-      className={cn(
-        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20 w-full rounded-r-lg",
-        isActive &&
-          "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700 "
-      )}
-    >
-      <div className="flex items-center gap-x-2 py-4">
-        <Icon
-          size={22}
-          className={cn("text-slate-500", isActive && "text-sky-700")}
-        />
-        {label}
-      </div>
-      <div
+    <SheetClose asChild>
+      <button
+        onClick={onClick}
+        type="button"
         className={cn(
-          "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
-          isActive && "opacity-100"
+          "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20 w-full rounded-r-lg",
+          isActive &&
+            "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700 "
         )}
-      />
-    </button>
+      >
+        <div className="flex items-center gap-x-2 py-4">
+          <Icon
+            size={22}
+            className={cn("text-slate-500", isActive && "text-sky-700")}
+          />
+          {label}
+        </div>
+        <div
+          className={cn(
+            "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+            isActive && "opacity-100"
+          )}
+        />
+      </button>
+    </SheetClose>
   );
 };
