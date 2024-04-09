a198e70451630e19770eba93a744e7e4ad53361b
souvikdeb2612 <souvikdeb26@gmail.com>
2024-04-09 20:18:18 +0530

    fix: join class inactive button issue

--- src/app/admin/courses/[courseId]/_components/classCard.tsx
+++ src/app/admin/courses/[courseId]/_components/classCard.tsx
@@ -20,6 +20,7 @@ function ClassCard({
     classData: any
     classType: any
 }) {
+    console.log('first', classType)
     return (
         <Card className="w-full border-none shadow p-6" key={classData.id}>
             <div className="flex items-center justify-between truncate">
