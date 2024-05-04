4f32aa6c668056974866f8d9e612d5ae5aa35de6
psd9988 <psd9988@gmail.com>
2024-05-04 16:17:34 +0530

    refactored tiptap.tsx

--- src/app/admin/courses/[courseId]/module/_components/Article/Tiptap.tsx
+++ src/app/admin/courses/[courseId]/module/_components/Article/Tiptap.tsx
@@ -3,19 +3,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import {
-    Bold,
-    Strikethrough,
-    Italic,
-    List,
-    ListOrdered,
-    Minus,
-    Undo,
-    Redo,
-    Code,
-    Heading1Icon,
-    LucideUnderline,
-} from 'lucide-react'
+
 import { zodResolver } from '@hookform/resolvers/zod'
 import { FieldValues, FormProvider, useForm } from 'react-hook-form'
 import * as z from 'zod'
@@ -40,6 +28,7 @@ import ListItem from '@tiptap/extension-list-item'
 import BulletList from '@tiptap/extension-bullet-list'
 import OrderedList from '@tiptap/extension-ordered-list'
 import Text from '@tiptap/extension-text'
+import TextEditorBtns from './TextEditorBtns'
 
 const extensions = [
     StarterKit,
@@ -88,7 +77,6 @@ interface TiptapProps {
 
 const Tiptap = ({ chapterContent }: TiptapProps) => {
     const [title, setTitle] = useState('')
-    const [isAlertOpen, setIsAlertOpen] = useState(false)
     const { register } = useForm()
 
     const editor = useEditor({
@@ -141,12 +129,6 @@ const Tiptap = ({ chapterContent }: TiptapProps) => {
         getArticleContent()
     }, [chapterContent])
 
-    const activeButtonStyles =
-        'text-white flex items-center justify-center rounded-md  bg-green-900  focus:outline-none mr-2'
-
-    const inActiveBtnStyles =
-        'text-white flex items-center justify-center rounded-md  bg-secondary  focus:outline-none mr-2'
-
     // Define your form schema
     const formSchema = z.object({
         title: z.string().min(2, {
@@ -164,137 +146,9 @@ const Tiptap = ({ chapterContent }: TiptapProps) => {
     return (
         <div>
             <div className="toolbar-btns flex justify-center">
-                <Button
-                    onClick={() => editor?.chain().focus().toggleBold().run()}
-                    disabled={!editor?.can().chain().focus().toggleBold().run()}
-                    className={`${
-                        editor?.isActive('bold')
-                            ? activeButtonStyles
-                            : inActiveBtnStyles
-                    }`}
-                >
-                    <Bold />
-                </Button>
-                <Button
-                    onClick={() => editor?.chain().focus().toggleItalic().run()}
-                    disabled={
-                        !editor?.can().chain().focus().toggleItalic().run()
-                    }
-                    className={
-                        editor?.isActive('italic')
-                            ? activeButtonStyles
-                            : inActiveBtnStyles
-                    }
-                >
-                    <Italic />
-                </Button>
-                <Button
-                    onClick={() => editor?.chain().focus().toggleStrike().run()}
-                    disabled={
-                        !editor?.can().chain().focus().toggleStrike().run()
-                    }
-                    className={
-                        editor?.isActive('strike')
-                            ? activeButtonStyles
-                            : inActiveBtnStyles
-                    }
-                >
-                    <Strikethrough />
-                </Button>
-                <Button
-                    onClick={() =>
-                        editor?.chain().focus().toggleUnderline().run()
-                    }
-                    className={
-                        editor?.isActive('underline')
-                            ? activeButtonStyles
-                            : inActiveBtnStyles
-                    }
-                >
-                    <LucideUnderline />
-                </Button>
-                <Button
-                    onClick={() => editor?.chain().focus().toggleCode().run()}
-                    disabled={!editor?.can().chain().focus().toggleCode().run()}
-                    className={
-                        editor?.isActive('code')
-                            ? activeButtonStyles
-                            : inActiveBtnStyles
-                    }
-                >
-                    <Code />
-                </Button>
-
-                <Button
-                    onClick={() =>
-                        editor
-                            ?.chain()
-                            .focus()
-                            .toggleHeading({ level: 1 })
-                            .run()
-                    }
-                    className={
-                        editor?.isActive('heading', { level: 1 })
-                            ? activeButtonStyles
-                            : inActiveBtnStyles
-                    }
-                >
-                    <Heading1Icon />
-                </Button>
-
-                <Button
-                    onClick={() =>
-                        editor?.chain().focus().toggleBulletList().run()
-                    }
-                    className={
-                        editor?.isActive('bulletList')
-                            ? activeButtonStyles
-                            : inActiveBtnStyles
-                    }
-                >
-                    <List />
-                </Button>
-                <Button
-                    onClick={() =>
-                        editor?.chain().focus().toggleOrderedList().run()
-                    }
-                    className={
-                        editor?.isActive('orderedList')
-                            ? activeButtonStyles
-                            : inActiveBtnStyles
-                    }
-                >
-                    <ListOrdered />
-                </Button>
-
-                <Button
-                    onClick={() =>
-                        editor?.chain().focus().setHorizontalRule().run()
-                    }
-                    className="mr-2"
-                >
-                    <Minus />
-                </Button>
-                <Button
-                    onClick={() => editor?.chain().focus().setHardBreak().run()}
-                    className="mr-2"
-                >
-                    hard break
-                </Button>
-                <Button
-                    onClick={() => editor?.chain().focus().undo().run()}
-                    disabled={!editor?.can().chain().focus().undo().run()}
-                    className="mr-2"
-                >
-                    <Undo />
-                </Button>
-                <Button
-                    onClick={() => editor?.chain().focus().redo().run()}
-                    disabled={!editor?.can().chain().focus().redo().run()}
-                >
-                    <Redo />
-                </Button>
+                <TextEditorBtns />
             </div>
+
             <div className="flex justify-center my-5">
                 <Form {...form}>
                     <form
