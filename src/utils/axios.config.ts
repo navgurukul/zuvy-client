4f4a6ed3c3fbdc0c94020401524f076bc03fe4b6
d3drock <61801183+d3drock@users.noreply.github.com>
2024-05-14 18:42:48 +0530

    axios fix

--- src/utils/axios.config.ts
+++ src/utils/axios.config.ts
@@ -4,7 +4,6 @@ import axios, { AxiosRequestConfig } from 'axios'
 
 const mainURL = process.env.NEXT_PUBLIC_MAIN_URL
 const apiURL = process.env.NEXT_PUBLIC_API_URL
-const localURL = process.env.NEXT_PUBLIC_LOCAL_URL
 
 let headers: AxiosRequestConfig['headers'] = {
     'Content-Type': 'application/json',
@@ -12,7 +11,6 @@ let headers: AxiosRequestConfig['headers'] = {
 
 const api = axios.create({
     baseURL: mainURL,
-    // baseUrl: localURL,
     headers,
 })
 
