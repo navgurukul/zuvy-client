fe83494283def537532993a6adc35958dc15646f
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2024-06-14 14:51:27 +0530

    fix/student dashboard - Resume learning

--- src/app/student/_components/schedule.tsx
+++ src/app/student/_components/schedule.tsx
@@ -2,8 +2,9 @@
 
 import { useEffect, useState, useCallback } from 'react'
 // import { Calendar } from "@/components/ui/calendar";
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card } from '@/components/ui/card'
 import { cn, getAttendanceColorClass } from '@/lib/utils'
+import { ChevronRight, Video } from 'lucide-react'
 import { BookOpenText } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { api } from '@/utils/axios.config'
@@ -14,18 +15,20 @@ import Image from 'next/image'
 import SubmissionCard from '@/app/admin/courses/[courseId]/_components/SubmissionCard'
 
 interface ResumeCourse {
-    bootcamp_name?: string
-    module_name?: string
+    bootcampName?: string
+    moduleName?: string
     bootcampId?: number
+    newChapter?: any
     moduleId?: number
 }
 type ScheduleProps = React.ComponentProps<typeof Card>
 
 function Schedule({ className, ...props }: ScheduleProps) {
-    // const [courseStarted, setCourseStarted] = useState<boolean>(false)
-    // const { studentData } = useLazyLoadedStudentData()
-    // const userID = studentData?.id && studentData?.id
-    // const [resumeCourse, setResumeCourse] = useState<ResumeCourse>({})
+    const [courseStarted, setCourseStarted] = useState<boolean>(false)
+    const { studentData } = useLazyLoadedStudentData()
+    const userID = studentData?.id && studentData?.id
+    const [resumeCourse, setResumeCourse] = useState<ResumeCourse>({})
+    const [nextChapterId, setNextChapterId] = useState([])
     const [upcomingClasses, setUpcomingClasses] = useState([])
     const [ongoingClasses, setOngoingClasses] = useState([])
     const [attendenceData, setAttendenceData] = useState<any[]>([])
@@ -63,27 +66,26 @@ function Schedule({ className, ...props }: ScheduleProps) {
 
     // useEffect(() => {}, [upcomingClasses, ongoingClasses, completedClasses])
 
-    // useEffect(() => {
-    //     const getResumeCourse = async () => {
-    //         try {
-    //             const response = await api.get(
-    //                 `/tracking/latest/learning/${userID}`
-    //             )
-    //             setResumeCourse(response.data)
-    //             // If we get res, then course started, hence courseStarted: true;
-    //             setCourseStarted(true)
-    //         } catch (error) {
-    //             console.error('Error getting resume course:', error)
-    //             if (
-    //                 (error as any)?.response?.data?.message ===
-    //                 `Cannot read properties of undefined (reading 'moduleId')`
-    //             ) {
-    //                 setCourseStarted(false)
-    //             }
-    //         }
-    //     }
-    //     if (userID) getResumeCourse()
-    // }, [userID])
+    useEffect(() => {
+        const getResumeCourse = async () => {
+            try {
+                const response = await api.get('/tracking/latestUpdatedCourse')
+                setResumeCourse(response.data)
+                setNextChapterId(response.data.newChapter.id)
+                // If we get res, then course started, hence courseStarted: true;
+                setCourseStarted(true)
+            } catch (error) {
+                console.error('Error getting resume course:', error)
+                if (
+                    (error as any)?.response?.data?.message ===
+                    `Cannot read properties of undefined (reading 'moduleId')`
+                ) {
+                    setCourseStarted(false)
+                }
+            }
+        }
+        if (userID) getResumeCourse()
+    }, [userID])
 
     const getUpcomingClassesHandler = useCallback(async () => {
         await api.get(`/student/Dashboard/classes/`).then((res) => {
@@ -187,34 +189,68 @@ function Schedule({ className, ...props }: ScheduleProps) {
                             </div>
                         </div>
                     )}
-
-                    {/* <Card className="text-start">
-                    <CardHeader className="bg-muted">
-                        <CardTitle>Pick up where you left</CardTitle>
-                    </CardHeader>
-                    <CardContent className="p-3 grid gap-4">
-                    <div className="flex flex-wrap items-center p-4 justify-between gap-8">
-                        <div className="flex items-center">
-                            <BookOpenText className="hidden sm:block" />
-                            <div className="flex-1 ml-2 space-y-1">
-                                <p className="text-sm font-medium leading-none">
-                                {resumeCourse?.bootcamp_name}
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                    {resumeCourse.module_name}
-                                </p>
-                            </div>
-                        </div>
-                        <Link
-                            href={`/student/courses/${resumeCourse?.bootcampId}/modules/${resumeCourse.moduleId}`}
-                        >
-                            <Button>Continue</Button>
-                            </Link>
-                            </div>
-                            </CardContent>
-                    </Card> */}
                 </div>
             </div>
+
+            {/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */}
+
+            {resumeCourse && (
+                <div className="flex flex-col flex-start mt-6">
+                    <h1 className="text-xl p-1 text-start font-bold">
+                        Start From Where You Left Off
+                    </h1>
+                    <div className="flex flex-row justify-between gap-6">
+                        <div className="flex flex-col">
+                            <div className="w-[800px]">
+                                <Card className="w-full mb-3 border-none shadow p-6">
+                                    <div className="flex flex-row justify-between gap-6">
+                                        <div>
+                                            <div className="flex flex-row gap-6">
+                                                {/* <Video size={25} /> */}
+                                                <BookOpenText className="hidden sm:block mt-2" />
+                                                <h1 className="text-lg p-1 text-start font-bold">
+                                                    Video - Intro to Variables
+                                                </h1>
+                                            </div>
+                                            <div className="flex flex-row gap-6">
+                                                <p className="text-md text-start mt-3 mb-2 text-center">
+                                                    {resumeCourse?.bootcampName}
+                                                </p>
+                                                <span className="w-2 h-2 bg-gray-500 rounded-full mt-5"></span>
+                                                <p className="text-md text-start mt-3 mb-2 text-center">
+                                                    {resumeCourse?.moduleName}
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="text-end">
+                                            <Button
+                                                variant={'ghost'}
+                                                className="text-xl font-bold"
+                                            >
+                                                <Link
+                                                    className="gap-3 flex items-center text-secondary"
+                                                    // href={`/student/courses/${resumeCourse?.bootcampId}/modules/${resumeCourse.moduleId}`}
+                                                    href={{
+                                                        pathname: `/student/courses/${resumeCourse?.bootcampId}/modules/${resumeCourse.moduleId}`,
+                                                        query: {
+                                                            nextChapterId,
+                                                        },
+                                                    }}
+                                                >
+                                                    <p>Resume Learning</p>
+                                                    <ChevronRight size={15} />
+                                                </Link>
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </Card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */}
 
             {/* <Calendar
         mode="single"
@@ -222,11 +258,10 @@ function Schedule({ className, ...props }: ScheduleProps) {
         onSelect={setDate}
         className="rounded-md border"
       /> */}
-            <div className="flex flex-col flex-start">
+            <div className="flex flex-col flex-start mt-6">
                 <h1 className="text-xl p-1 text-start font-bold">
                     Upcoming Submissions
                 </h1>
-                {/* <div className="w-[800px]"> */}
                 <div className="flex flex-row">
                     {submission.length > 0 ? (
                         submission.map((data) => {
@@ -243,7 +278,6 @@ function Schedule({ className, ...props }: ScheduleProps) {
                                 height={240}
                             />
                             <p className="text-lg mt-3 text-center">
-                                {/* <p className="text-md font-semibold"> */}
                                 There are no upcoming Submission
                             </p>
                         </div>
