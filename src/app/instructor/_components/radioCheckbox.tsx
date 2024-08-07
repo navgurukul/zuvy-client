9c30b58164c7a99c07c16f25d221eabb8afb13b2
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2024-08-07 16:10:11 +0530

    Instructor session page UI and API integration

--- src/app/instructor/_components/radioCheckbox.tsx
+++ src/app/instructor/_components/radioCheckbox.tsx
@@ -1,55 +1,136 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from 'react'
 
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Checkbox } from '@/components/ui/checkbox'
+import { Label } from '@/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { api } from '@/utils/axios.config'
+import { getCourseData, setStoreBatchValue } from '@/store/store'
 
-const RadioCheckbox: React.FC = () => {
-  let batches = true;
-  return (
-    <div className='flex items-center justify-between p-3 w-1/2'>
-      {batches && (
-        <div className=' flex  flex-col gap-y-3 items-start'>
-          <h1 className='font-semibold'>Batches</h1>
-          <div className='flex items-center space-x-2'>
-            <Checkbox id='terms' />
-            <label
-              htmlFor='terms'
-              className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-            >
-              AFE Batch 1
-            </label>
-            <Checkbox id='terms' />
-            <label
-              htmlFor='terms'
-              className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-            >
-              AFE Batch 2
-            </label>
-          </div>
+import { ComboboxStudent } from '@/app/admin/courses/[courseId]/(courseTabs)/students/components/comboboxStudentDataTable'
+import { Combobox } from '@/components/ui/combobox'
+
+export interface RadioCheckboxProps {
+    fetchSessions: (data: any) => void
+}
+
+const RadioCheckbox: React.FC<RadioCheckboxProps> = ({
+    fetchSessions,
+}: RadioCheckboxProps) => {
+    // let batches = true
+    const [batches, setBatches] = useState<any[]>([])
+    const [batchId, setBatchId] = useState<any>()
+    const [timeFrame, setTimeFrame] = useState<string>('all')
+    const { setbatchValueData } = setStoreBatchValue()
+
+    const handleComboboxChange = (value: string) => {
+        setBatchId(value)
+        setbatchValueData(value)
+    }
+
+    console.log('batchId', batchId)
+    const getBatches = useCallback(async () => {
+        try {
+            const response = await api.get(`/instructor/batchOfInstructor`)
+            console.log('response', response.data.data)
+            const transformedData = response.data.data.map(
+                (item: { batchId: any; batchName: any }) => ({
+                    value: item.batchId.toString(),
+                    label: item.batchName,
+                })
+            )
+            setBatchId(response.data.data[0].batchId)
+            // setbatchValueData(transformedData[0])
+            console.log('transformedData[0]', transformedData[0])
+            console.log('transformedData', transformedData)
+            setBatches(transformedData)
+        } catch (error) {
+            console.error('Error fetching courses:', error)
+        }
+    }, [])
+
+    const getSessions = useCallback(async () => {
+        try {
+            const response = await api.get(
+                `/instructor/getAllUpcomingClasses?limit=10&offset=0&timeFrame=${timeFrame}&batchId=${batchId}`
+            )
+            console.log('response', response.data.data.responses)
+            fetchSessions(response.data.data.responses)
+            // const transformedData = response.data.data.map(
+            //     (item: { batchId: any; batchName: any }) => ({
+            //         value: item.batchId.toString(),
+            //         label: item.batchName,
+            //     })
+            // )
+            // setBatchId(response.data.data[0].batchId)
+            // // setbatchValueData(transformedData[0])
+            // console.log('transformedData[0]', transformedData[0])
+            // console.log('transformedData', transformedData)
+            // setBatches(transformedData)
+        } catch (error) {
+            console.error('Error fetching courses:', error)
+        }
+    }, [batchId, timeFrame])
+
+    useEffect(() => {
+        getBatches()
+    }, [getBatches])
+
+    useEffect(() => {
+        getSessions()
+    }, [batchId, timeFrame])
+
+    console.log('timeFrame', timeFrame)
+
+    return (
+        <div className="flex items-center justify-between p-3 w-1/2">
+            {batches && (
+                <div className=" flex  flex-col gap-y-3 items-start">
+                    <h1 className="font-semibold">Batches</h1>
+                    <Combobox
+                        data={batches}
+                        title={'Batch'}
+                        onChange={handleComboboxChange}
+                        batch={batches.length > 0}
+                        batchChangeData={batches[0]}
+                    />
+                </div>
+            )}
+            <div className="flex  flex-col gap-y-3 items-start">
+                <h1 className="font-semibold">Time Period</h1>
+                <div className="flex items-center space-x-2">
+                    <RadioGroup
+                        className="flex items-center "
+                        defaultValue="comfortable"
+                    >
+                        <div className="flex  space-x-2">
+                            <RadioGroupItem
+                                value="comfortable"
+                                id="r1"
+                                onClick={() => setTimeFrame('all')}
+                            />
+                            <Label htmlFor="r1">All Time</Label>
+                        </div>
+                        <div className="flex  space-x-2">
+                            <RadioGroupItem
+                                value="default"
+                                id="r2"
+                                onClick={() => setTimeFrame('1 week')}
+                            />
+                            <Label htmlFor="r2">1 Week</Label>
+                        </div>
+                        <div className="flex  space-x-2">
+                            <RadioGroupItem
+                                value="compact"
+                                id="r3"
+                                onClick={() => setTimeFrame('2 week')}
+                            />
+                            <Label htmlFor="r3">2 Weeks</Label>
+                        </div>
+                    </RadioGroup>
+                </div>
+            </div>
         </div>
-      )}
-      <div className='flex  flex-col gap-y-3 items-start'>
-        <h1 className='font-semibold'>Time Period</h1>
-        <div className='flex items-center space-x-2'>
-          <RadioGroup className='flex items-center ' defaultValue='comfortable'>
-            <div className='flex  space-x-2'>
-              <RadioGroupItem value='default' id='r1' />
-              <Label htmlFor='r1'>All Time</Label>
-            </div>
-            <div className='flex  space-x-2'>
-              <RadioGroupItem value='comfortable' id='r2' />
-              <Label htmlFor='r2'>1 Week</Label>
-            </div>
-            <div className='flex  space-x-2'>
-              <RadioGroupItem value='compact' id='r3' />
-              <Label htmlFor='r3'>2 Weeks</Label>
-            </div>
-          </RadioGroup>
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default RadioCheckbox;
+export default RadioCheckbox
--- src/app/instructor/page.tsx
+++ src/app/instructor/page.tsx
@@ -1,52 +1,77 @@
-"use client";
-import React from "react";
+'use client'
+import React, { useState, useEffect, useCallback } from 'react'
 
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import InstructorCard from "./_components/instructorCard";
-import RadioCheckbox from "./_components/radioCheckbox";
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import InstructorCard from './_components/instructorCard'
+import RadioCheckbox from './_components/radioCheckbox'
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 
-import { useLazyLoadedStudentData } from "@/store/store";
+import { useLazyLoadedStudentData } from '@/store/store'
 
 const InstructorPage = () => {
-  const { studentData } = useLazyLoadedStudentData();
-  const username: string[] | undefined = studentData?.name?.split(" ");
-  const newUserName: string | undefined =
-    username?.[0]?.charAt(0)?.toUpperCase() +
-    (username?.[0]?.slice(1)?.toLowerCase() || "");
-  let batches = true;
-  const arr1 = [1, 2, 3, 4, 5];
-  return (
-    <MaxWidthWrapper>
-      <div className='flex items-center justify-start'>
-        <Avatar>
-          {/* <AvatarImage src={studentData?.profile_picture} /> */}
-        </Avatar>{" "}
-        <p className='text-[30px]'>
-          {`Hi, ${newUserName}! Here's Your Schedule`}
-        </p>
-      </div>
-      <RadioCheckbox />
-      <div className='p-3'>
-        <h1 className='text-start font-semibold '>5 Upcoming Classes </h1>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4'>
-          {arr1.map((item) => {
-            return (
-              <InstructorCard
-                key={item}
-                batchName={"AFE + Navgurukul Basics"}
-                date1={26}
-                month={"Jan"}
-                topicTitle={"Intro to Variables"}
-                classesTiming={"4:00 PM - 5:00 PM"}
-                typeClass={"Join"}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </MaxWidthWrapper>
-  );
-};
+    const { studentData } = useLazyLoadedStudentData()
+    const [ongoingSessions, setOngoingSessions] = useState<any[]>([])
+    const [upcomingSessions, setUpcomingSessions] = useState<any[]>([])
+    const username: string[] | undefined = studentData?.name?.split(' ')
+    const newUserName: string | undefined =
+        username?.[0]?.charAt(0)?.toUpperCase() +
+        (username?.[0]?.slice(1)?.toLowerCase() || '')
+    let batches = true
+    const arr1 = [1, 2, 3, 4, 5]
 
-export default InstructorPage;
+    const fetchSessions = (data: any) => {
+        setUpcomingSessions(data.ongoing)
+        setUpcomingSessions(data.upcoming)
+    }
+
+    console.log('upcomingSessions', upcomingSessions)
+
+    return (
+        <MaxWidthWrapper className="">
+            <div className="flex items-center justify-start">
+                <Avatar>
+                    <AvatarImage src={studentData?.profile_picture} />
+                </Avatar>
+                <p className="text-[30px] ml-4">
+                    {`Hi, ${newUserName}! Here's Your Schedule`}
+                </p>
+            </div>
+            <RadioCheckbox fetchSessions={fetchSessions} />
+            <div className="p-3">
+                <h1 className="text-start font-semibold ">
+                    5 Upcoming Classes{' '}
+                </h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-8 gap-y-6">
+                    {ongoingSessions.map((item) => {
+                        return (
+                            <InstructorCard
+                                key={item.batchId}
+                                batchName={item.bootcampName}
+                                topicTitle={item.title}
+                                startTime={item.startTime}
+                                endTime={item.endTime}
+                                typeClass={item.status}
+                                classLink={item.hangoutLink}
+                            />
+                        )
+                    })}
+                    {upcomingSessions.map((item) => {
+                        return (
+                            <InstructorCard
+                                key={item.batchId}
+                                batchName={item.bootcampName}
+                                topicTitle={item.title}
+                                startTime={item.startTime}
+                                endTime={item.endTime}
+                                typeClass={item.status}
+                                classLink={item.hangoutLink}
+                            />
+                        )
+                    })}
+                </div>
+            </div>
+        </MaxWidthWrapper>
+    )
+}
+
+export default InstructorPage
