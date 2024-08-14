3f4748b43012f8a05717acf445d7fee385a4a333
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2024-08-14 13:42:44 +0530

    recording class in instructor

--- src/app/instructor/recording/page.tsx
+++ src/app/instructor/recording/page.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useEffect, useCallback } from 'react'
+import { OFFSET, POSITION } from '@/utils/constant'
 
 import { Input } from '@/components/ui/input'
 import RadioCheckbox from '../_components/radioCheckbox'
@@ -6,6 +9,19 @@ import InstructorCard from '../_components/instructorCard'
 
 const Recordings = () => {
     const arr1 = [1, 2, 3, 4, 5]
+    const [ongoingSessions, setOngoingSessions] = useState<any[]>([])
+    const [upcomingSessions, setUpcomingSessions] = useState<any[]>([])
+    const [position, setPosition] = useState(POSITION)
+    const [pages, setPages] = useState<number>()
+    const [offset, setOffset] = useState<number>(OFFSET)
+    const [currentPage, setCurrentPage] = useState<number>(1)
+    const [totalSessions, setTotalSessions] = useState<number>(0)
+    const [lastPage, setLastPage] = useState<number>(0)
+
+    const fetchSessions = (data: any) => {
+        setUpcomingSessions(data.ongoing)
+        setUpcomingSessions(data.upcoming)
+    }
     return (
         <div>
             <h1 className="text-start text-lg font-semibold ">
@@ -15,6 +31,14 @@ const Recordings = () => {
                 type="search"
                 placeholder="Search By Name"
                 className="w-1/5"
+            />
+            <RadioCheckbox
+                fetchSessions={fetchSessions}
+                offset={offset}
+                position={position}
+                setTotalSessions={setTotalSessions}
+                setPages={setPages}
+                setLastPage={setLastPage}
             />
             {/* <RadioCheckbox />
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4'>
