d994dfe32aa8ae5aaa017c11ba0ad470c60e59d5
d3drock <priyomjeet12345@gmail.com>
2024-08-30 14:59:11 +0530

    Merge branch 'dev' of https://github.com/navgurukul/zuvy-client into bugfix/getclassesAPIandAssignment

--- src/app/admin/courses/[courseId]/(courseTabs)/submissions/components/AssesmentSubmission.tsx
+++ src/app/admin/courses/[courseId]/(courseTabs)/submissions/components/AssesmentSubmission.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useCallback, useEffect, useState } from 'react'
 import AssesmentComponent from '../../../_components/AssesmentComponent'
 import { api } from '@/utils/axios.config'
