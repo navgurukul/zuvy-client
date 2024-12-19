24e3098b892b2dcf5022a6561e70456225d54569
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2024-12-19 15:44:53 +0530

    Fix sumbissions

--- src/app/admin/courses/[courseId]/_components/submissionComponent.tsx
+++ src/app/admin/courses/[courseId]/_components/submissionComponent.tsx
@@ -99,10 +99,10 @@ const SubmissionComponent = (props: Props) => {
                     </h1>
                     <div className="relative group">
                         <button
-                            className={`ml-2 cursor-pointer ${
+                            className={`ml-2 ${
                                 isDisabled
-                                    ? 'text-gray-400'
-                                    : 'text-gray-500 hover:text-gray-700'
+                                    ? 'text-gray-400 cursor-no-drop'
+                                    : 'text-gray-500 hover:text-gray-700 cursor-pointer'
                             }`}
                             onClick={isDisabled ? undefined : handleDownloadPdf}
                             aria-label="Download full report"
