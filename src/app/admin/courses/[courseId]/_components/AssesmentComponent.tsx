92802e4ced03bf28aa383d38b432ea699afa7090
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2024-12-20 17:35:19 +0530

    Fix Assessment card

--- src/app/admin/courses/[courseId]/_components/AssesmentComponent.tsx
+++ src/app/admin/courses/[courseId]/_components/AssesmentComponent.tsx
@@ -112,7 +112,7 @@ const AssesmentComponent = (props: Props) => {
     return (
         <div
             ref={printRef}
-            className="lg:flex-row h-auto lg:h-[220px] sm:h-[360px] w-full shadow-lg my-5 rounded-lg p-4 lg:p-6 bg-white dark:bg-gray-800 transition-transform transform hover:shadow-xl"
+            className="lg:flex-row h-auto lg:h-[290px] sm:h-[360px] w-full shadow-lg my-5 rounded-lg p-4 lg:p-6 bg-white dark:bg-gray-800 transition-transform transform hover:shadow-xl"
         >
             <div className="w-full justify-between py-2 lg:mx-4 min-h-[250px] sm:min-h-[200px]">
                 <div className="flex items-center justify-between">
@@ -207,42 +207,36 @@ const AssesmentComponent = (props: Props) => {
                             Qualified
                         </p>
                     </div>
-                    <div className="flex justify-end mt-auto">
-                        {props.studentsSubmitted > 0 ? (
-                            <Link
-                                href={`/admin/courses/${props.bootcampId}/submissionAssesments/${props.id}`}
-                                className="w-full lg:w-auto"
-                            >
-                                <div className="flex justify-center items-center w-full lg:w-auto py-2 text-secondary font-bold rounded-md transition-all duration-300">
-                                    <Button variant="ghost">
-                                        <h1 className="w-full text-center flex lg:text-right">
-                                            View Submission
-                                            <ChevronRight
-                                                size={20}
-                                                className="ml-2"
-                                            />
-                                        </h1>
-                                    </Button>
-                                </div>
-                            </Link>
-                        ) : (
-                            <div className="flex justify-center cursor-no-drop items-center w-full lg:w-auto py-2 text-secondary font-bold rounded-md transition-all duration-300">
-                                <Button
-                                    variant="ghost"
-                                    disabled={props.studentsSubmitted === 0} // Disable button if no submissions
-                                >
-                                    <h1 className="w-full text-center flex lg:text-right">
-                                        View Submission
-                                        <ChevronRight
-                                            size={20}
-                                            className="ml-2"
-                                        />
-                                    </h1>
-                                </Button>
-                            </div>
-                        )}
-                    </div>
                 </div>
+            </div>
+            <div className="flex justify-end mt-auto">
+                {props.studentsSubmitted > 0 ? (
+                    <Link
+                        href={`/admin/courses/${props.bootcampId}/submissionAssesments/${props.id}`}
+                        className="w-full lg:w-auto"
+                    >
+                        <div className="flex justify-center items-center w-full lg:w-auto py-2 text-secondary font-bold rounded-md transition-all duration-300">
+                            <Button variant="ghost">
+                                <h1 className="w-full text-center flex lg:text-right">
+                                    View Submission
+                                    <ChevronRight size={20} className="ml-2" />
+                                </h1>
+                            </Button>
+                        </div>
+                    </Link>
+                ) : (
+                    <div className="flex justify-center cursor-no-drop items-center w-full lg:w-auto py-2 text-secondary font-bold rounded-md transition-all duration-300">
+                        <Button
+                            variant="ghost"
+                            disabled={props.studentsSubmitted === 0} // Disable button if no submissions
+                        >
+                            <h1 className="w-full text-center flex lg:text-right">
+                                View Submission
+                                <ChevronRight size={20} className="ml-2" />
+                            </h1>
+                        </Button>
+                    </div>
+                )}
             </div>
         </div>
     )
