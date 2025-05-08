b84fadca3e1c6b315fd1f87e19747c0e4490fbd9
Prem <102803078+giribabu22@users.noreply.github.com>
2025-05-08 13:18:11 +0530

    Update axios.config.ts

--- src/utils/axios.config.ts
+++ src/utils/axios.config.ts
@@ -2,7 +2,7 @@
 
 import axios, { AxiosRequestConfig } from 'axios'
 
-const mainUrl = process.env.NEXT_PUBLIC_MAIN_URL
+const mainUrl =  'https://main-api.zuvy.org'
 // const mainUrl = "http://zuvy.navgurukul.org/"
 const apiURL = process.env.NEXT_PUBLIC_API_URL
 const localUrl = process.env.NEXT_PUBLIC_LOCAL_URL
