59794e641d76e4f5fbb490eb99b743b74ccc881b
psd9988 <psd9988@gmail.com>
2025-07-03 02:11:57 +0530

    delete old ref code

--- oldStudentSideRef/getAssessmentStore.tsx
+++
@@ -1,6 +0,0 @@
-// export const getAssessmentStore = create<assessmentStore>((set) => ({
-//     fullScreenExitInstance: 0,
-//     setFullScreenExitInstance: (newValue: number) => {
-//         set({ fullScreenExitInstance: newValue })
-//     },
-// }))
