8868de7da8aa75d38a215f46c3c45cee5f719293
d3drock <priyomjeet12345@gmail.com>
2025-09-03 14:21:51 +0530

    Hotfixesfor-quiz-assessment-view-results

--- src/app/student/_components/chapter-content/AssessmentContent.tsx
+++ src/app/student/_components/chapter-content/AssessmentContent.tsx
@@ -480,7 +480,7 @@ const AssessmentContent: React.FC<AssessmentContentProps> = ({ chapterDetails, o
                       </p>
 
                       <p className="font-semibold text-center">
-                        Your Score: {Math.trunc(percentage) || 0}/100
+                        Your Score: {(percentage.toFixed(2)) || 0}/100
                       </p>
                     </div>
                   </div>
