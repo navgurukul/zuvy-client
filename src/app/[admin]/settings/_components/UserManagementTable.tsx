4f7c12e6615f4c97f3583d06d8672550f3a386b1
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2025-11-17 16:36:35 +0530

    RBAC

--- src/app/[admin]/settings/_components/UserManagementTable.tsx
+++ src/app/[admin]/settings/_components/UserManagementTable.tsx
@@ -70,6 +70,7 @@ export function UserManagementTable<TData extends User, TValue>({
                 ...user,
                 id: user.userId, // Use userId as id for SearchBox
             }))
+            console.log('Fetched suggestions:', suggestions)
 
             return suggestions
         } catch (error) {
